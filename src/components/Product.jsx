b1f439ed8533c3e3c2de5fda784a47fc9a806b25
Kumar Akshat <kakshat247@gmail.com>
2024-01-22 19:38:41 +0530

    completed the styling of the Home page and Product component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -22,23 +22,28 @@ function Product({product}) {
     }
 
   return (
-    <div>
-        <div>{product.title}</div>
+    <div className='flex flex-col items-center justify-between hover:scale-110 transition duration-300 ease-in shadow-[0_3px_10px_rgb(0,0,0,0.2)] gap-3 p-4 mt-10 ml-5 rounded-xl'>
         <div>
-            <p>{product.description}</p>
+            <p className='text-gray-700 font-semibold text-lg text-left truncate w-40 mt-1'>{product.title}</p>
+        </div>
+        <div>
+            <p className='w-40 text-gray-400 font-normal text-[10px] text-left'>{product.description.split(" ").slice(0,10).join(" ") + "..."}</p>
         </div>
 
-        <div>
-            <img src={product.image}></img>
+        <div className='h-[180px]'>
+            <img src={product.image} className='h-full w-full'></img>
         </div>
 
-        <div>
-            <p>{product.price}</p>
-        </div>
+        <div className='w-full flex justify-between gap-11 mt-5'>
 
-        {
-            cart.some((p) => p.id === product.id) ? (<button onClick={removeFromCart}>Remove Item</button>) : (<button onClick={addToCart}>Add to Cart</button>)
-        }
+            <div>
+                <p className='text-green-600 font-bold'>₹{product.price}</p>
+            </div>
+
+            {
+                cart.some((p) => p.id === product.id) ? (<button onClick={removeFromCart} className='text-gray-700 border-2 border-gray-700 rounded-full font-semibold text-[8px]p-1 px-3 uppercase hover:bg-gray-700 hover:text-white transition duration-300 ease-in'>Remove Item</button>) : (<button onClick={addToCart} className='text-gray-700 border-2 border-gray-700 rounded-full font-semibold text-[8px]p-1 px-3 uppercase  hover:bg-gray-700 hover:text-white transition duration-300 ease-in'>Add to Cart</button>)
+            }
+        </div>
     </div>
   )
 }
