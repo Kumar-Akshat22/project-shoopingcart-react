676100d1e73dccb1417443bfa0a2d6d0e0ace2bd
Kumar Akshat <kakshat247@gmail.com>
2024-01-23 00:04:18 +0530

    added more styling to the Cart component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -15,10 +15,10 @@ function Cart() {
   } , [cart]);
 
   return (
-    <div className='min-h-[80vh] max-w-6xl mx-auto flex justify-center items-center'>
+    <div className='min-h-[80vh] max-w-6xl mx-auto flex justify-center'>
       {
         cart.length > 0 ?
-        (<div className='w-full flex items-center'>
+        (<div className='w-full flex justify-center'>
           
           <div className='w-[60%] flex flex-col p-2'>
             {
@@ -26,18 +26,21 @@ function Cart() {
             }
           </div>
 
-          <div className='w-[40%] h-[100%] flex flex-col mt-5 jusitfy-between'>
-            <div className='flex flex-col gap-5'>
-              <div className='text-xl uppercase text-green-800 font-semibold'>Your Cart</div>
-              <div className='uppercase text-4xl font-bold text-green-700 -mt-5'>Summary</div>
-              <p className='text-xl'>
-              <span className='font-semibold text-xl text-gray-700 mt-2'>Total Items: </span>{cart.length}
-              </p>
-            </div>
+          <div className='w-[40%] flex flex-col mt-5 '>
+            
+            <div className='h-[100%] flex flex-col p-5 gap-5 justify-between my-14'>
+              <div className='flex flex-col gap-5'>
+                <div className='text-xl uppercase text-green-800 font-semibold'>Your Cart</div>
+                <div className='uppercase text-4xl font-bold text-green-700 -mt-5'>Summary</div>
+                <p className='text-xl'>
+                <span className='font-semibold text-xl text-gray-700 mt-2'>Total Items: </span>{cart.length}
+                </p>
+              </div>
 
-            <div className='w-full flex flex-col gap-5'>
-              <p className='text-xl font-bold'><span className='font-semibold text-gray-700'>Total Amount:</span> <span>₹{totalAmount}</span></p>
-              <button className='w-full p-4 border-2 border-green-600 bg-green-600 tracking-wider text-white rounded-lg font-bold hover:bg-purple-50 hover:text-green-700 transition duration-300'>Checkout Now</button>
+              <div className='w-full flex flex-col gap-5'>
+                <p className='text-xl font-bold'><span className='font-semibold text-gray-700'>Total Amount:</span> <span>₹{totalAmount}</span></p>
+                <button className='w-full p-4 border-2 border-green-600 bg-green-600 tracking-wider text-white rounded-lg font-bold hover:bg-purple-50 hover:text-green-700 transition duration-300'>Checkout Now</button>
+              </div>
             </div>
           </div>
 
